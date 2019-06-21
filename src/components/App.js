119f7b79472d437531099e55bd7f9430df18684f
jakehurrell <hakejurrell@gmail.com>
2019-06-21 19:39:37 -0400

    hooks and functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import ResourceList from './resourceList';
 
 const App = () => {
-  console.log(useState('posts'));
   const [resource, setResource] = useState('posts');
   return (
     <div>
@@ -9,7 +9,7 @@ const App = () => {
         <button onClick={() => setResource('posts')}>Posts</button>
         <button onClick={() => setResource('todos')}>Todos</button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
   );
 }
